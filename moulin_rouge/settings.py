96dc01100f0d61cdd92169e2b20aa978608bf278
panos <pntine@precisionmicrodrives.com>
2022-02-20 18:57:11 +0000

    correction

--- moulin_rouge/settings.py
+++ moulin_rouge/settings.py
@@ -24,7 +24,7 @@ PROJECT_DIR = os.path.join(BASE_DIR, "moulin_rouge")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-os.environ.get('SECRET_KEY')
+os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
